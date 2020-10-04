bf02a64ec506bd2217fcd493c80dd97a565ede49
Doug F <forddf12@outlook.com>
2020-10-04 15:19:09 -0500

    ready to submit

---
+++ src/org/launchcode/java/studios/FunWIthQuizes/src/TrueFalse.java
@@ -0,0 +1,19 @@
+public class TrueFalse extends Question {
+
+    private boolean correctAnswer;
+
+    public TrueFalse(String prompt, boolean correctAnswer) {
+        super (prompt);
+        this.options.add("True");
+        this.options.add("False");
+        this.correctAnswer = correctAnswer;
+    }
+
+    @Override
+    public boolean isCorrect(String answer) {
+        int answerOption = Integer.parseInt(answer);
+        String givenAnswer = options.get(answerOption -1);
+        return givenAnswer.toLowerCase().equals("true") == correctAnswer;
+    }
+
+}
